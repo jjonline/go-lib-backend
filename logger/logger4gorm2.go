e493dbac5a06eb43db2f99d7e5014e7702db767b
Jea杨 <jjonline@jjonline.cn>
2021-06-16 17:09:49 +0800

    update go mod

--- logger/logger4gorm2.go
+++ logger/logger4gorm2.go
@@ -25,9 +25,9 @@ func (g gorm2Logger) LogMode(level logger.LogLevel) logger.Interface {
 }
 
 // Info 输出info级别的日志
-// gorm里只有一些添加callback处理函数的钩子时触发<譬如update时自定义钩子>，内部调整为启用debug级别才记录日志
+// gorm里只有一些添加callback处理函数的钩子时触发<譬如update时自定义钩子>
 func (g gorm2Logger) Info(ctx context.Context, s string, i ...interface{}) {
-	if g.logger.Core().Enabled(zap.DebugLevel) {
+	if g.logger.Core().Enabled(g.level) {
 		g.logger.Info(
 			"gorm.log",
 			zap.String("module", "gorm.info"),
@@ -38,10 +38,10 @@ func (g gorm2Logger) Info(ctx context.Context, s string, i ...interface{}) {
 }
 
 // Warn 输出警告日志
-// 1、gorm里移除、重排callback处理函数的钩子时触发<譬如update时自定义钩子>，内部调整为启用debug级别才记录日志
-// 2、自定义model不匹配时，内部调整为启用debug级别才记录日志
+// 1、gorm里移除、重排callback处理函数的钩子时触发<譬如update时自定义钩子>
+// 2、自定义model不匹配时
 func (g gorm2Logger) Warn(ctx context.Context, s string, i ...interface{}) {
-	if g.logger.Core().Enabled(zap.DebugLevel) {
+	if g.logger.Core().Enabled(g.level) {
 		switch len(i) {
 		case 2:
 			g.logger.Info(
@@ -62,9 +62,9 @@ func (g gorm2Logger) Warn(ctx context.Context, s string, i ...interface{}) {
 }
 
 // Error 输出出错日志
-// gorm内部一些执行的错误：初始化db、解析schema、解析自定义回调钩子、dryRun出错时，内部调整为启用info级别才记录日志
+// gorm内部一些执行的错误：初始化db、解析schema、解析自定义回调钩子、dryRun出错时
 func (g gorm2Logger) Error(ctx context.Context, s string, i ...interface{}) {
-	if g.logger.Core().Enabled(zap.InfoLevel) {
+	if g.logger.Core().Enabled(g.level) {
 		g.logger.Info(
 			"gorm.log",
 			zap.String("module", "gorm.info"),
@@ -74,9 +74,9 @@ func (g gorm2Logger) Error(ctx context.Context, s string, i ...interface{}) {
 }
 
 // Trace 输出trace级别日志
-// gorm内部主要用于输出执行的sql日志，调整为启用debug级别才输出日志
+// gorm内部主要用于输出执行的sql日志
 func (g gorm2Logger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
-	if g.logger.Core().Enabled(zap.DebugLevel) {
+	if g.logger.Core().Enabled(g.level) {
 		elapsed := time.Since(begin) // 执行耗时
 		sql, rows := fc()            // 执行的sql和影响行数
 
@@ -86,16 +86,16 @@ func (g gorm2Logger) Trace(ctx context.Context, begin time.Time, fc func() (stri
 				zap.String("module", "gorm.sql"),
 				zap.Error(err), // 注意：执行查询未找到记录也会在此
 				zap.String("sql", sql),
-				zap.Int64("affect_rows", rows),
+				zap.Int64("affected_rows", rows),
 				zap.String("source", utils.FileWithLineNum()),
 				zap.Duration("duration", elapsed),
 			)
 		} else {
-			g.logger.Error(
+			g.logger.Info(
 				"gorm.log",
 				zap.String("module", "gorm.sql"),
 				zap.String("sql", sql),
-				zap.Int64("affect_rows", rows),
+				zap.Int64("affected_rows", rows),
 				zap.String("source", utils.FileWithLineNum()),
 				zap.Duration("duration", elapsed),
 			)
@@ -109,5 +109,13 @@ func NewGorm2Logger() logger.Interface {
 		panic(errors.New("please use logger.New() create logger.Logger instance at first"))
 	}
 
-	return gorm2Logger{logger: *zapLogger}
+	return gorm2Logger{
+		logger: *zapLogger,
+		levelMap: map[logger.LogLevel]zapcore.Level{
+			logger.Silent: zapcore.ErrorLevel, // gorm日志级别静默则zap到不低于error才记录
+			logger.Error:  zapcore.InfoLevel,  // gorm日志错误则zap到不低于info才记录
+			logger.Warn:   zapcore.InfoLevel,  // gorm日志警告则zap到不低于info才记录
+			logger.Info:   zapcore.DebugLevel, // gorm日志信息则zap到不低于debug才记录
+		},
+	}
 }
