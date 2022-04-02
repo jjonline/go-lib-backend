3a03c20b419b708b6b8a54f73658afeca0d93d74
Jea杨 <jjonline@jjonline.cn>
2022-04-02 14:34:49 +0800

    fix: crond log

--- crond/logger.go
+++ crond/logger.go
@@ -14,17 +14,19 @@ type cronLog struct {
 // Info 信息级别日志输出
 func (l cronLog) Info(msg string, keysAndValues ...interface{}) {
 	// record
-	fields := []zapcore.Field{}
+	var fields []zapcore.Field
+	fields = append(fields, zap.String("module", module))
 	for key, val := range keysAndValues {
 		if "now" == val {
 			fields = append(fields, zap.Any("execute_time", keysAndValues[key+1]))
 		}
 		if "entry" == val {
 			entryID := keysAndValues[key+1]
-			fields = append(fields, zap.Any("entry_id", entryID))
+			entryIntID := int(entryID.(cron.EntryID))
+			fields = append(fields, zap.Int("entry_id", entryIntID))
 
 			// 取当前command的签名添加日志字段
-			if command, exist := registeredCommand[int(entryID.(cron.EntryID))]; exist {
+			if command, exist := registeredCommand[entryIntID]; exist {
 				fields = append(fields, zap.String("signature", command.Signature()))
 			}
 		}
@@ -41,8 +43,8 @@ func (l cronLog) Info(msg string, keysAndValues ...interface{}) {
 
 // Error 错误级别日志输出
 func (l cronLog) Error(err error, msg string, keysAndValues ...interface{}) {
-	fields := []zapcore.Field{}
-	fields = append(fields, zap.Error(err))
+	var fields []zapcore.Field
+	fields = append(fields, zap.String("module", module), zap.Error(err))
 	for key, val := range keysAndValues {
 		if "stack" == val {
 			fields = append(fields, zap.Any("stack", keysAndValues[key+1]))
--- example/queue/go.mod
+++ example/queue/go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/go-redis/redis/v8 v8.8.3
 	github.com/jjonline/go-lib-backend/queue v0.0.0-20210720014741-ee748420702a
-	go.uber.org/zap v1.18.1
+	go.uber.org/zap v1.21.0
 )
 
 replace github.com/jjonline/go-lib-backend/queue => ../../queue
