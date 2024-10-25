f659f5d7f1b0992ca9a20c47933ebaa0aea91fcd
Jea杨 <jjonline@jjonline.cn>
2024-10-25 17:34:56 +0800

    feat: crawler tool

--- crawler/chrome.go
+++ crawler/chrome.go
@@ -2,6 +2,7 @@ package crawler
 
 import (
 	"context"
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/device"
@@ -26,11 +27,12 @@ type Chrome struct {
 	infoLogger  func(string, ...interface{}) // 普通日志
 
 	// setting
-	ua              string       // UA头
-	proxyUrl        string       // 代理地址，为空则无代理
-	headless        bool         // 是否无头模式，即打开chrome是否要显示界面，默认false不显示
-	emulate         *device.Info // 自定义仿真参数，UA头宽高尺寸Touch特性等，默认依据请求UA头智能使用 PcEmulate H5Emulate
-	_screenshotPath string       // 单次使用的截图存储路径
+	ua              string            // UA头
+	proxyUrl        string            // 代理地址，为空则无代理
+	headless        bool              // 是否无头模式，即打开chrome是否要显示界面，默认false不显示
+	emulate         *device.Info      // 自定义仿真参数，UA头宽高尺寸Touch特性等，默认依据请求UA头智能使用 PcEmulate H5Emulate
+	_screenshotPath string            // 单次使用的截图存储路径
+	cookies         []chromedp.Action // 设置随请求发送的cookie
 }
 
 // New 初始化
@@ -42,6 +44,7 @@ func New() *Chrome {
 		headless:        false,
 		emulate:         nil,
 		_screenshotPath: "",
+		cookies:         nil,
 	}
 }
 
@@ -88,6 +91,46 @@ func (c *Chrome) SetInfoLogger(f func(string, ...interface{})) *Chrome {
 	return c
 }
 
+// SetCookie 设置随请求发送出去的cookie
+//   - key      cookie键名
+//   - value    cookie键值
+//   - domain   cookie的有效域名，例如：.a.com 或 www.a.com
+//   - path     path路径，例如：/
+//   - expired  cookie有效时长
+//   - httpOnly 是否httpOnly
+//   - secure   是否secure安全
+func (c *Chrome) SetCookie(key, value, domain, path string, expired time.Duration, httpOnly, secure bool) *Chrome {
+	cookie := chromedp.ActionFunc(func(ctx context.Context) error {
+		expr := cdp.TimeSinceEpoch(time.Now().Add(expired))
+		err := network.SetCookie(key, value).
+			WithExpires(&expr).
+			WithDomain(domain).
+			WithPath(path).
+			WithHTTPOnly(httpOnly).
+			WithSecure(secure).
+			Do(ctx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	// add cookie
+	if nil == c.cookies {
+		c.cookies = make([]chromedp.Action, 0)
+	}
+	c.cookies = append(c.cookies, cookie)
+
+	return c
+}
+
+// CleanCookie 清理启动时设置的cookie
+func (c *Chrome) CleanCookie() *Chrome {
+	c.cookies = nil
+	return c
+}
+
 // SetCaptureScreenshot 设置截图存储文件夹路径
 //
 //   - savePath 存储截图的文件夹路径，路径需是存在可读写的， 例如 ./runtime/
@@ -131,6 +174,11 @@ func (c *Chrome) SetFromUA(userAgentFrom string) *Chrome {
 //   - node     打开网页指标元素ID，以等待网页已经渲染完毕
 //   - task     补充的操作
 //   - timeout  可选的超时时长，不传或传0表示发出一个请求不超时
+//
+// 返回值说明
+//   - string            如果有html内容，则html字符串
+//   - *network.Response chromedp原生resp
+//   - error             是否报错
 func (c *Chrome) Execute(target, node string, task chromedp.Tasks, timeout ...time.Duration) (string, *network.Response, error) {
 	var (
 		html    string
@@ -148,12 +196,13 @@ func (c *Chrome) Execute(target, node string, task chromedp.Tasks, timeout ...ti
 
 	// 通用任务设置
 	tasks := chromedp.Tasks{
+		disableAutomationDetect(),
 		chromedp.Emulate(emulate), // UA头
 		// browser.SetDownloadBehavior(browser.SetDownloadBehaviorBehaviorDeny), // 禁用自动文件下载
 		chromedp.Navigate(target), // chrome打开目标网页
 	}
 	if node != "" {
-		tasks = append(tasks, chromedp.WaitVisible(node, chromedp.ByID))
+		tasks = append(tasks, WaitRootNodeVisible(node))
 	}
 	tasks = append(
 		tasks,
@@ -166,6 +215,11 @@ func (c *Chrome) Execute(target, node string, task chromedp.Tasks, timeout ...ti
 		tasks = append(tasks, chromedp.CaptureScreenshot(&png))
 	}
 
+	// 如果有cookie，写cookie应当在打开url之前
+	if nil != c.cookies {
+		tasks = append(c.cookies, tasks...)
+	}
+
 	// +++++++++++++++++++++++
 	// 初始化chrome
 	// +++++++++++++++++++++++
@@ -176,12 +230,14 @@ func (c *Chrome) Execute(target, node string, task chromedp.Tasks, timeout ...ti
 	// +++++++++++++++++++++++
 	// 构造context
 	// +++++++++++++++++++++++
-	if len(timeout) <= 0 || 0 == timeout[0] {
-		// 构造无超时时长的context
-		c.makeCancelContext()
-	} else {
-		// 构造指定超时时长的context
-		c.makeTimeoutContext(timeout[0])
+	if nil == c.Ctx {
+		if len(timeout) <= 0 || 0 == timeout[0] {
+			// 构造无超时时长的context
+			c.makeCancelContext()
+		} else {
+			// 构造指定超时时长的context
+			c.makeTimeoutContext(timeout[0])
+		}
 	}
 
 	// +++++++++++++++++++++++
@@ -190,7 +246,7 @@ func (c *Chrome) Execute(target, node string, task chromedp.Tasks, timeout ...ti
 	resp, err := chromedp.RunResponse(c.Ctx, tasks...)
 
 	// 最后取消请求体context
-	defer c.cancelRequest()
+	// defer c.cancelRequest()
 
 	// 如果要求截图则保存截图
 	if c._screenshotPath != "" && nil != png {
@@ -212,6 +268,9 @@ func (c *Chrome) openChrome() *Chrome {
 		// 是否headless模式--true则没有浏览器界面 false则有一个浏览器界面
 		// 打开浏览器界面便于调试，非开发环境则是headless模式
 		chromedp.Flag("headless", c.headless),
+		chromedp.Flag("enable-automation", false),
+		chromedp.Flag("remote-debugging-port", "9222"),
+		chromedp.Flag("window-size", "1920,1080"),
 	}
 	options = append(chromedp.DefaultExecAllocatorOptions[:], options...)
 
--- crawler/helper.go
+++ crawler/helper.go
@@ -3,8 +3,63 @@ package crawler
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	randV2 "math/rand/v2"
 	"os"
+	"time"
 )
+
+// pseudoRandFloat64 [0.0,1.0)范围随机数
+func pseudoRandFloat64() float64 {
+	return randV2.Float64()
+}
+
+// RandFloat64 生成 [0,max) 范围内的float64类型的随机数
+func RandFloat64(max int64) float64 {
+	return float64(randV2.Int64N(max)) + randV2.Float64()
+}
+
+// RandFloat64MinMax 生成 [min,max) 范围内的float64类型的随机数
+func RandFloat64MinMax(min, max int64) float64 {
+	// 最大最小一致，永远返回一致值
+	if min == max {
+		return float64(min)
+	}
+
+	// 最大最小颠倒，调换一下
+	if min > max {
+		mid := max
+		max = min
+		min = mid
+	}
+	return RandFloat64(max-min) + float64(min)
+}
+
+// RandInt64 生成 [0,max) 范围内的int64类型随机数
+func RandInt64(max int64) int64 {
+	return randV2.Int64N(max)
+}
+
+// RandInt64MinMax 区间 [min,max)
+func RandInt64MinMax(min, max int64) int64 {
+	// 最大最小一致，永远返回一致值
+	if min == max {
+		return min
+	}
+
+	// 最大最小颠倒，调换一下
+	if min > max {
+		mid := max
+		max = min
+		min = mid
+	}
+
+	return RandInt64(max-min) + min
+}
+
+// RandInt64MinMaxDuration 区间 [min,max)时长随机秒
+func RandInt64MinMaxDuration(min, max int64) time.Duration {
+	return time.Duration(RandFloat64MinMax(min, max)) * time.Second
+}
 
 // 判断文件夹是否存在
 func pathExists(path string) bool {
